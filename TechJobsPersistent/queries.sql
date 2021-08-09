9c24871ec027c155da4776a6920bf5f022aff6a1
Mike <mike.sierra.7950@gmail.com>
2021-08-09 15:03:07 -0400

    Added data type descriptions to queries.sql

--- TechJobsPersistent/queries.sql
+++ TechJobsPersistent/queries.sql
@@ -1,5 +1,27 @@
 --Part 1
 
+--jobs
+	--	Id INT PRIMARY KEY AUTO_INCREMENT
+	--	Name LONGTEXT
+	--	EmployerId INT
+	--	FOREIGN KEY (EmployerId) REFERENCES employers(Id)
+
+--employers
+	--	Id INT PRIMARY KEY AUTO_INCREMENT
+	--	Name LONGTEXT
+	--	Location LONGTEXT
+
+--skills
+	--	Id INT PRIMARY KEY AUTO_INCREMENT
+	--	Name LONGTEXT
+	--	Description LONGTEXT
+
+--jobSkills
+	--	JobId INT 
+	--	SkillId INT
+	--	FOREIGN KEY (JobId) REFERENCES jobs(Id)
+	--	FOREIGN KEY (SkillId) REFERENCES skills(Id)
+
 --Part 2
 
 --Part 3

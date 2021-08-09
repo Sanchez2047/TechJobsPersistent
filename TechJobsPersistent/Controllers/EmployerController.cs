5a338268cf7f4136c2d1b34bb85028f7670c0c71
Mike <mike.sierra.7950@gmail.com>
2021-08-09 15:38:28 -0400

    Added ability to add, view, and see details of employers

--- TechJobsPersistent/Controllers/EmployerController.cs
+++ TechJobsPersistent/Controllers/EmployerController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TechJobsPersistent.Data;
 using TechJobsPersistent.Models;
 using TechJobsPersistent.ViewModels;
 
@@ -12,24 +13,43 @@ namespace TechJobsPersistent.Controllers
 {
     public class EmployerController : Controller
     {
+        private JobDbContext _context;
+        public EmployerController(JobDbContext jobDbContext)
+        {
+            this._context = jobDbContext;
+        }
         // GET: /<controller>/
         public IActionResult Index()
         {
+            List<Employer> employers = new List<Employer>(_context.Employers.ToList());
             return View();
         }
 
         public IActionResult Add()
         {
-            return View();
+            AddEmployerViewModel addEmployerViewModel = new AddEmployerViewModel();
+            return View(addEmployerViewModel);
         }
 
-        public IActionResult ProcessAddEmployerForm()
+        public IActionResult ProcessAddEmployerForm(AddEmployerViewModel addEmployerViewModel)
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+                Employer employer = new Employer
+                {
+                    Name = addEmployerViewModel.Name,
+                    Location = addEmployerViewModel.Location
+                };
+                _context.Employers.Add(employer);
+                _context.SaveChanges();
+                return Redirect("Index");
+            }
+            return View(addEmployerViewModel);
         }
 
         public IActionResult About(int id)
         {
+            Employer employer = _context.Employers.Find(id);
             return View();
         }
     }

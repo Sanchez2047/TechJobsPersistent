544bd2da92a4140241c6f04997a91081821aa50e
Mike <mike.sierra.7950@gmail.com>
2021-08-10 12:58:28 -0400

    Added Part 3 SQL task to program

--- TechJobsPersistent/queries.sql
+++ TechJobsPersistent/queries.sql
@@ -1,37 +1,44 @@
 --Part 1
 
 
---jobs
-	--	Id INT PRIMARY KEY AUTO_INCREMENT
-	--	Name LONGTEXT
-	--	EmployerId INT
-	--	FOREIGN KEY (EmployerId) REFERENCES employers(Id)
+	--jobs
+		--	Id INT PRIMARY KEY AUTO_INCREMENT
+		--	Name LONGTEXT
+		--	EmployerId INT
+		--	FOREIGN KEY (EmployerId) REFERENCES employers(Id)
 
---employers
-	--	Id INT PRIMARY KEY AUTO_INCREMENT
-	--	Name LONGTEXT
-	--	Location LONGTEXT
+	--employers
+		--	Id INT PRIMARY KEY AUTO_INCREMENT
+		--	Name LONGTEXT
+		--	Location LONGTEXT
 
---skills
-	--	Id INT PRIMARY KEY AUTO_INCREMENT
-	--	Name LONGTEXT
-	--	Description LONGTEXT
+	--skills
+		--	Id INT PRIMARY KEY AUTO_INCREMENT
+		--	Name LONGTEXT
+		--	Description LONGTEXT
 
---jobSkills
-	--	JobId INT 
-	--	SkillId INT
-	--	FOREIGN KEY (JobId) REFERENCES jobs(Id)
-	--	FOREIGN KEY (SkillId) REFERENCES skills(Id)
+	--jobSkills
+		--	JobId INT 
+		--	SkillId INT
+		--	FOREIGN KEY (JobId) REFERENCES jobs(Id)
+		--	FOREIGN KEY (SkillId) REFERENCES skills(Id)
 
 --Part 2
 
 
---SELECT Name 
---FROM employers
---WHERE Location = "St. Louis City";
+	--SELECT Name 
+	--FROM employers
+	--WHERE Location = "St. Louis City";
 
 
 --Part 3
+
+	--SELECT Name, description
+	--FROM skills
+	--LEFT JOIN jobskills ON skills.id = jobskills.SkillId
+	--WHERE jobskills.SkillId IS NOT NULL
+	--ORDER BY Name ASC;
+
 
 
 
